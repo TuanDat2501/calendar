32b3e1cf5da78fef6c9ded84de95746c2a375b18
TuanDat2501 <169238329+TuanDat2501@users.noreply.github.com>
2025-12-31 10:58:19 +0700

    fix icon

--- manifest.ts
+++ manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#2563eb',
     icons: [
       {
-        src: 'https://cdn-icons-png.flaticon.com/512/3652/3652191.png',
+        src: 'Calendar.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: 'https://cdn-icons-png.flaticon.com/512/3652/3652191.png',
+        src: 'Calendar.png',
         sizes: '512x512',
         type: 'image/png',
       },
